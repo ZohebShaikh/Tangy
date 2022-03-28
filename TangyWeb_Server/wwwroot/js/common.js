80c35a6104c81f5e67f762a69be16075475fbdf3
Zoheb Shaikh <26975142+ZohebShaikh@users.noreply.github.com>
2022-03-29 00:59:56 +0530

    added Create, Read, Update and Delete functionality

--- TangyWeb_Server/wwwroot/js/common.js
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -23,3 +23,10 @@ window.SweetAlert = (type, message) => {
     )
   }
 }
+
+function ShowDeleteConfirmationModal() {
+  $('#deleteConfirmationModal').modal('show');
+}
+function HideDeleteConfirmationModal() {
+  $('#deleteConfirmationModal').modal('hide');
+}
