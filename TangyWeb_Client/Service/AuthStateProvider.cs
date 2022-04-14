6454de7e4a3ed8f1df5bfb4d3eca6b7f4c02a386
Zoheb Shaikh <26975142+ZohebShaikh@users.noreply.github.com>
2022-04-14 16:29:57 +0530

    added registeration service

---
+++ TangyWeb_Client/Service/AuthStateProvider.cs
@@ -0,0 +1,34 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using Tangy_Common;
+using TangyWeb_Client.Helper;
+
+namespace TangyWeb_Client.Service
+{
+  public class AuthStateProvider:AuthenticationStateProvider
+  {
+    private readonly HttpClient _httpClient;
+    private readonly ILocalStorageService _localstorage;
+
+    public AuthStateProvider(HttpClient httpClient,ILocalStorageService localStorage)
+    {
+      _httpClient = httpClient;
+      _localstorage = localStorage;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+      var token = await _localstorage.GetItemAsync<string>(StaticDetails.Local_Token);
+      if (token == null)
+      {
+        return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+      }
+
+      _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer",token);
+
+      return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(JwtParser.ParseClaimsFromJwt(token),"jwtAuthType")));
+    }
+  }
+}
