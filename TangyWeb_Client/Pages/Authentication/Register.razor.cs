eb463abc87313fd72772d9d49c1efbc7af8f3e96
Zoheb Shaikh <26975142+ZohebShaikh@users.noreply.github.com>
2022-04-14 16:35:29 +0530

    moved register code to code behind

---
+++ TangyWeb_Client/Pages/Authentication/Register.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using Tangy_Models;
+using TangyWeb_Client.Service.IService;
+
+namespace TangyWeb_Client.Pages.Authentication
+{
+  public partial class Register
+  {
+    private SignUpRequestDTO SignUpRequest = new();
+    public bool IsProcessing { get; set; } = false;
+    public bool ShowRegisterationErrors { get; set; }
+    public IEnumerable<string> Errors { get; set; }
+
+    [Inject] 
+    public IAuthenticationService _authService { get; set; }
+
+    [Inject]
+    public NavigationManager _navigationManager { get; set; }
+    private async Task RegisterUser()
+    {
+      ShowRegisterationErrors = false;
+      IsProcessing = true;
+      var result = await _authService.RegisterUser(SignUpRequest);
+
+      if (result.IsRegisterationSuccessful)
+      {
+        //registeration is successful
+        _navigationManager.NavigateTo("/login");
+      }
+      else
+      {
+        //failure
+        Errors = result.Errors;
+        ShowRegisterationErrors = true;
+      }
+    }
+  }
+}
